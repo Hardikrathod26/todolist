07fb846a2a57c545351f240cead0b05cf651e27f
Hardikrathod26 <{hardikr26@gmail.com}>
2022-12-02 11:09:32 +0100

    vercel json file added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -139,6 +140,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, () => {
+  console.log("Server started on port ${PORT}");
 });
